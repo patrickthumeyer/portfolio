e8bfe0f01dd0d794a4f75dd6936e6ef9786de15a
patrickthumeyer <52319164+patrickthumeyer@users.noreply.github.com>
2019-07-11 11:48:24 +0200

    Add files via upload

---
+++ Portfolio_final_js.js
@@ -0,0 +1,33 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener("DOMContentLoaded",() => {
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+const top = document.getElementById("myBtn");
+top.addEventListener("click",topFunction,false)
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+const submit = document.getElementsByClassName("submitJS")[0];
+submit.addEventListener("click",confirmation,false)
+function confirmation() {
+    Swal.fire({
+    position: 'center', 
+    type: 'success',  
+    title: 'Your request has been submitted',
+    showConfirmButton: false,
+    timer: 2000
+})   
+}
+})
